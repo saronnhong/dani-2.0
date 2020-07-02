3f590777eea726b8c85ee6c208367d47c1d3ef73
Saron <saronnhong@gmail.com>
2020-07-02 12:15:49 -0700

    set up enabledScreens, custom font loading, and folder structure

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import Navigator from './navigation/Navigator';
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if(!fontLoaded) {
+    return (
+      <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ readme.md
@@ -0,0 +1,7 @@
+### Starting a React Native App
+## Steps
+1. expo init <name of app>
+2. Install Navigators
+    - npm install --save react-navigation-stack
+    - npm install --save react-navigation-tabs
+    - npm install --save react-navigation-drawer
