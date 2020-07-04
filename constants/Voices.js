c2d737bef5edc32d543ad92683f0064e01878502
Saron <saronnhong@gmail.com>
2020-07-04 11:00:10 -0700

    add speech options

---
+++ constants/Voices.js
@@ -0,0 +1,11 @@
+export default {
+    aaron: 'com.apple.ttsbundle.siri_male_en-US_compact',
+    fred: 'com.apple.speech.synthesis.voice.Fred',
+    nicky: 'com.apple.ttsbundle.siri_female_en-US_compact',
+    samantha: 'com.apple.ttsbundle.Samantha-compact',
+    alex: 'com.apple.speech.voice.Alex'
+    
+}
+
+//list of voices
+//https://gist.github.com/Koze/d1de49c24fc28375a9e314c72f7fdae4
