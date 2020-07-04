9c647b0bf356d7c499eb59c2076c63fc94003fa3
Saron <saronnhong@gmail.com>
2020-07-03 19:57:27 -0700

    clean up on speech in favs

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
-import Constants from 'expo-constants';
 import * as Speech from 'expo-speech';
 
-export default class FavoritesScreen extends React.Component {
-    speak() {
-        var thingToSay = 'And before anyone starts to panic, oh my god he is making videos in place of writing, OF COURSE I am still working on WINDS OF WINTER as well. ';
-        Speech.speak(thingToSay);
-    }
+const FavoritesScreen = () => {
+    const thingToSay = 'And before anyone starts to panic, oh my god he is making videos in place of writing, OF COURSE I am still working on WINDS OF WINTER as well. ';
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Button title="Press to hear some words" onPress={this.speak} />
-            </View>
-        );
-    }
-}
+    return (
+        <View style={styles.container}>
+            <Button title="Press to hear some words" onPress={() => Speech.speak(thingToSay)} />
+        </View>
+    )
+
+};
 
 FavoritesScreen.navigationOptions = navData => {
     return {
@@ -37,14 +32,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        paddingTop: Constants.statusBarHeight,
-        backgroundColor: '#ecf0f1',
-        padding: 8,
-    },
-    paragraph: {
-        margin: 24,
-        fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
+    }
 });
+
+export default FavoritesScreen;
