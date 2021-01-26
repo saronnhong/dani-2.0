0769fc722dfe6ceefd1a2a598108d200c014e454
Saron <saronnhong@gmail.com>
2021-01-25 19:44:02 -0800

    add styling to most used page

--- screens/SpeechBoard/MostUsedScreen.js
+++ screens/SpeechBoard/MostUsedScreen.js
@@ -1,26 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, TextInput, TouchableOpacity, Image, ScrollView, Alert } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import Colors from '../../constants/Colors';
 import { WORDS } from '../../data/words';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/HeaderButton';
-import * as wordActions from '../../store/actions/sentenceBar'
+import { RECORDING_OPTION_IOS_BIT_RATE_STRATEGY_CONSTANT } from 'expo-av/build/Audio';
 
-const MostUsedScreen = props => {
+const MostUsedScreen = () => {
     let savedDictionary = useSelector(num => num.count.sentenceCount);
-    const [sortable, setSortable] = useState([])
+    const [sortable, setSortable] = useState([]);
+    const [dictionary, setDictionary] = useState({});
+
+    let sortThis = () => {
+        setDictionary(savedDictionary);
+        setSortable(Object.entries(dictionary).sort((a, b) => b[1] - a[1]));
+    }
 
     useEffect(() => {
-        setSortable(Object.entries(savedDictionary).sort((a, b) => b[1] - a[1]));
+        sortThis();
         console.log(sortable[0]);  //returns sorted array
-    }, [savedDictionary]);
-
+        console.log(sortable.length + "length of dic")
+    }, [savedDictionary, dictionary]);
+    let tempNum = 1;
     return (
         <View style={styles.screen}>
-            {sortable.map(item =>
-                <Text key={item[0]} style={styles.listItems}>{item[0]}</Text>
-            )}
+            <Text style={styles.headLine}>Top 10 Most Used Sentences</Text>
+            {!sortable.length && <Text style={styles.emptySearch}>No results found.</Text>}
+            <View style={styles.listContainer}>
+                {sortable.map(item =>
+                    <Text key={item[0]} style={styles.listItems}>{tempNum++}. {item[0]}</Text>
+                )}
+            </View>
+
+
         </View>
     )
 };
@@ -45,8 +58,20 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'rgba(255, 185, 64, .2)',
     },
+    headLine: {
+        fontSize: 24,
+        fontFamily: 'roboto-bold',
+        marginVertical: 25
+    },
+    listContainer: {
+        alignItems: 'flex-start'
+    },
     listItems: {
-        fontSize: 24
+        fontSize: 20,
+        color: Colors.border
+    },
+    emptySearch: {
+        marginTop: 100
     }
 });
 export default MostUsedScreen;
