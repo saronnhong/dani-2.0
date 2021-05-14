aad1a1b35ba00921358f920b1028e5627c0c21e9
Saron <saronnhong@gmail.com>
2021-05-14 10:59:33 -0700

    add verbs section to words list. working on changing sign up page to class component to use refs

---
+++ data/verbs.js
@@ -0,0 +1,22 @@
+import Word from '../models/words';
+
+export const VERBS = [
+    new Word(
+        'v1',
+        'verbs',
+        'Is',
+        // require('../assets/images/speechboard/chat/hello.png')
+    ),
+    new Word(
+        'v2',
+        'verbs',
+        'A',
+        // require('../assets/images/speechboard/chat/hello.png')
+    ),
+    new Word(
+        'v3',
+        'verbs',
+        'An',
+        // require('../assets/images/speechboard/chat/hello.png')
+    ),
+];
