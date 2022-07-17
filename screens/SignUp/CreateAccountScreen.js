1ffd58ea2b839b8e7f4845217f790c7d4ff37f6f
Rebecca Schleimer <rschle@sas.upenn.edu>
2022-07-17 11:43:03 -0700

    settings createSettings troubleshoot

--- screens/SignUp/CreateAccountScreen.js
+++ screens/SignUp/CreateAccountScreen.js
@@ -55,7 +55,7 @@ class CreateAccountScreen extends Component {
                 age: 0
             }, () => this.submitData());
         } else {
-            console.log("grabbing a new state", this.state)
+            // console.log("grabbing a new state", this.state)
             this.props.navigation.navigate({
                 routeName: 'EnterPasswordScreen',
                 params: {
